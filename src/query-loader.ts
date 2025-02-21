c76412f7e31cdd9be8bdec7788d3c16953d39b33
Ivan Banha <banha.ivan@gmail.com>
2025-02-21 09:00:40 +0200

    feat: Add fragments support QueryLoader + tests

--- src/query-loader.ts
+++ src/query-loader.ts
@@ -1,66 +1,141 @@
-import { uniq } from 'es-toolkit';
-import { readFile, readFileSync } from 'fs';
-import * as glob from 'glob';
-import * as path from 'path';
+import { merge, uniq } from 'es-toolkit';
+import { readFileSync } from 'fs';
+import { sync } from 'glob';
+import { basename, extname } from 'path';
 
-export type LoaderConfig = {
-  /**
-   * List of paths to scan for sql templates
-   */
+export type QueryLoaderConfig = {
+  /** List of paths to scan for sql templates */
   templates: string[];
+
+  /** List of paths to scan for sql fragments */
+  fragments: string[];
 };
 
-export const defaultConfig: LoaderConfig = {
+export const defaultConfig: QueryLoaderConfig = {
   templates: [],
+  fragments: [],
+};
+
+const particles = {
+  template: '.template',
+  fragment: '.fragment',
 };
 
 /**
  * To Consider:
  *  - Implement naming strategies
  */
-export class QueryLoader {
-  private filePaths = new Map<string, string>();
-  private queries = new Map<string, string>();
+export class QueryLoader<T extends string, F extends string> {
+  private templatePaths = new Map<T, string>();
+  private fragmentPaths = new Map<F, string>();
 
-  constructor(private config: LoaderConfig = defaultConfig) {
-    this.scanTemplates();
+  private templates = new Map<T, string>();
+  private fragments = new Map<F, string>();
+
+  constructor(config: Partial<QueryLoaderConfig> = {}) {
+    const cfg = merge(defaultConfig, config);
+
+    this.validateConfig(cfg);
+    this.scanFolders(cfg);
   }
 
-  get filesCount() {
-    return this.filePaths.size;
+  get templatesCount() {
+    return this.templatePaths.size;
   }
 
-  hasTemplate(templateName: string) {
-    return this.filePaths.has(templateName);
+  get fragmentsCount() {
+    return this.fragmentPaths.size;
   }
 
-  getTemplate(templateName: string) {
-    if (!this.filePaths.has(templateName)) {
+  hasTemplate(templateName: T) {
+    return this.templatePaths.has(templateName);
+  }
+
+  hasFragment(fragmentName: F) {
+    return this.fragmentPaths.has(fragmentName);
+  }
+
+  getTemplate(templateName: T) {
+    if (!this.templatePaths.has(templateName)) {
       throw new Error(`Template "${templateName}" not found`);
     }
 
-    if (this.queries.has(templateName)) {
-      return this.queries.get(templateName);
+    if (this.templates.has(templateName)) {
+      return this.templates.get(templateName);
     }
 
-    const filePath = this.filePaths.get(templateName)!;
+    const filePath = this.templatePaths.get(templateName)!;
     const templateStr = readFileSync(filePath, { encoding: 'utf-8' });
 
-    this.queries.set(templateName, templateStr);
+    this.templates.set(templateName, templateStr);
 
     return templateStr;
   }
 
-  private scanTemplates() {
-    if (this.config.templates.length === 0) return;
+  getFragment(fragmentName: F) {
+    if (!this.fragmentPaths.has(fragmentName)) {
+      throw new Error(`Template "${fragmentName}" not found`);
+    }
 
-    const paths = uniq(this.config.templates);
-    const files = glob.sync(paths);
+    if (this.fragments.has(fragmentName)) {
+      return this.fragments.get(fragmentName);
+    }
+
+    const filePath = this.fragmentPaths.get(fragmentName)!;
+    const templateStr = readFileSync(filePath, { encoding: 'utf-8' });
+
+    this.fragments.set(fragmentName, templateStr);
+
+    return templateStr;
+  }
+
+  private scanFolders(config: QueryLoaderConfig) {
+    if (config.templates.length === 0 && config.fragments.length === 0) return;
+
+    const paths = uniq([...config.templates, ...config.fragments]);
+    const files = sync(paths);
 
     files.forEach((file) => {
-      const fileName = path.basename(file, path.extname(file));
+      const fileName = basename(file, extname(file));
+      const isFragment = fileName.match(particles.fragment);
 
-      this.filePaths.set(fileName, file);
+      if (isFragment) {
+        const fragmentName = fileName.replace(particles.fragment, '') as F;
+        this.fragmentPaths.set(fragmentName, file);
+
+        return;
+      }
+
+      const templateName = fileName.replace(particles.template, '') as T;
+      this.templatePaths.set(templateName, file);
+    });
+  }
+
+  private validateConfig(config: QueryLoaderConfig) {
+    config.templates.forEach((path) => {
+      const hasCorrectIdentifier = path.includes(particles.template + '.');
+
+      if (hasCorrectIdentifier) {
+        return;
+      }
+
+      throw new Error('Incorrect template path!', {
+        cause:
+          'A template file name should include the ".template" in it. E.g. "my-query.template.sql"',
+      });
+    });
+
+    config.fragments.forEach((path) => {
+      const hasCorrectIdentifier = path.includes(particles.fragment + '.');
+
+      if (hasCorrectIdentifier) {
+        return;
+      }
+
+      throw new Error('Incorrect fragment path!', {
+        cause:
+          'A fragment file name should include the ".fragment" in it. E.g. "my-fragment.fragment.sql"',
+      });
     });
   }
 }
--- tests/query-loader/query-loader.test.ts
+++ tests/query-loader/query-loader.test.ts
@@ -10,51 +10,82 @@ const queryTemplates = {
 
 const queries = {
   simpleQuery: 'simple-query',
-  conditionalQuery: 'conditional-query',
+  simpleQueryFragment: 'simple',
 };
 
 const templateFolderPath = join('tests', 'query-loader', 'templates');
 
 describe('Query Loader', () => {
-  test('Do not detect any files with empty "templates"', () => {
-    const loader = new QueryLoader({
-      templates: [],
-    });
+  test('Do not detect any files with empty config', () => {
+    const loader = new QueryLoader();
 
-    expect(loader.filesCount).toBe(0);
+    expect(loader.templatesCount).toBe(0);
   });
 
-  test('Detects all queries in a folder and its subfolders', () => {
+  test('Throws error if path incorrect', () => {
+    const throwTemplatesError = () => {
+      const loader = new QueryLoader({
+        templates: [join('tests', 'query-loader', '**', '*.sql')],
+        fragments: [join('tests', 'query-loader', '**', '*.fragment.sql')],
+      });
+    };
+
+    const throwFragmentsError = () => {
+      const loader = new QueryLoader({
+        templates: [join('tests', 'query-loader', '**', '*.template.sql')],
+        fragments: [join('tests', 'query-loader', '**', '*.sql')],
+      });
+    };
+
+    expect(throwTemplatesError).toThrowError();
+    expect(throwFragmentsError).toThrowError();
+  });
+
+  test('Detects templates and fragments in a folder and its subfolders', () => {
     const loader = new QueryLoader({
-      templates: [join('tests', 'query-loader', '**', '*.sql')],
+      templates: [join('tests', 'query-loader', '**', '*.template.sql')],
+      fragments: [join('tests', 'query-loader', '**', '*.fragment.sql')],
     });
 
-    expect(loader.filesCount).toBe(2);
+    expect(loader.templatesCount).toBe(2);
+    expect(loader.fragmentsCount).toBe(2);
   });
 
   test('Find all queries in specified folders', () => {
     const loader = new QueryLoader({
       templates: [
-        join(templateFolderPath, 'simple', '*.sql'),
-        join(templateFolderPath, 'conditional', '*.sql'),
+        join(templateFolderPath, 'simple', '*.template.sql'),
+        join(templateFolderPath, 'conditional', '*.template.sql'),
+      ],
+      fragments: [
+        join(templateFolderPath, 'simple', '*.fragment.sql'),
+        join(templateFolderPath, 'conditional', '*.fragment.sql'),
       ],
     });
 
-    expect(loader.filesCount).toBe(2);
+    expect(loader.templatesCount).toBe(2);
+    expect(loader.fragmentsCount).toBe(2);
   });
 
   test('Loads query from FS', () => {
     const loader = new QueryLoader({
       templates: [
-        join(templateFolderPath, 'simple', '*.sql'),
-        join(templateFolderPath, 'conditional', '*.sql'),
+        join(templateFolderPath, 'simple', '*.template.sql'),
+        join(templateFolderPath, 'conditional', '*.template.sql'),
+      ],
+      fragments: [
+        join(templateFolderPath, 'simple', '*.fragment.sql'),
+        join(templateFolderPath, 'conditional', '*.fragment.sql'),
       ],
     });
 
     expect(loader.hasTemplate(queries.simpleQuery)).toBeTruthy();
+    expect(loader.hasFragment(queries.simpleQueryFragment)).toBeTruthy();
 
     const templateStr = loader.getTemplate(queries.simpleQuery);
-
     expect(templateStr).toBe(queryTemplates.simpleQuery);
+
+    const fragmentStr = loader.getFragment(queries.simpleQueryFragment);
+    expect(fragmentStr).toBe(queryTemplates.simpleQuery);
   });
 });
